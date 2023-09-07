2b35a8bffd741548cb38ff748605c5c58680e6cc
1327605995@qq.com <1327605995@qq.com>
2023-09-07 22:50:13 +0800

    swagger

--- app/handler/api.go
+++ app/handler/api.go
@@ -9,6 +9,16 @@ import (
 	"github.com/spf13/cast"
 )
 
+// PutDesks
+// @Summary 保存卡组
+// @Schemes
+// @Description 保存卡组，如果卡组Id没有则创建。有id则覆盖数据库这条记录
+// @Tags 卡组
+// @Param data body models.Decks true "卡组信息"
+// @Accept json
+// @Produce json
+// @Success 200 {object} resp.SuccessResp
+// @Router /api/putDesks [post]
 func PutDesks(c *gin.Context) {
 	var desks models.Decks
 	err := c.ShouldBindJSON(&desks)
@@ -29,9 +39,19 @@ func PutDesks(c *gin.Context) {
 		resp.Fail(c, err.Error())
 		return
 	}
-	resp.Success(c, nil)
+	resp.Success(c, "ok")
 }
 
+// GetDesksById
+// @Summary 查询卡组
+// @Schemes
+// @Description 查询卡组id，获取卡组信息
+// @Tags 卡组
+// @Param data  query string true "id"
+// @Accept json
+// @Produce json
+// @Success 200 {object} resp.SuccessResp{data=models.Decks} "ok"
+// @Router /api/getDesksById [get]
 func GetDesksById(c *gin.Context) {
 	desksId := c.Query("id")
 	if desksId == "" {
@@ -42,10 +62,21 @@ func GetDesksById(c *gin.Context) {
 	desks, err := service.CardsService.GetDesksById(cast.ToUint64(desksId))
 	if err != nil {
 		resp.Fail(c, err.Error())
+		return
 	}
 	resp.Success(c, desks)
 }
 
+// GetDesksList
+// @Summary 查询卡组列表
+// @Schemes
+// @Description 查询用户的卡组列表
+// @Tags 卡组
+// @Param data  query string true "userId"
+// @Accept json
+// @Produce json
+// @Success 200 {object} resp.SuccessResp{data=[]models.Decks}
+// @Router /api/getDesksList [get]
 func GetDesksList(c *gin.Context) {
 	userId := c.Query("userId")
 	if userId == "" {

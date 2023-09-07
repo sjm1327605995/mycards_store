c55490c44e1e797fe94835c4b33d5cf6db88cb37
1327605995@qq.com <1327605995@qq.com>
2023-09-07 21:39:09 +0800

    防跨域

--- app/router/router.go
+++ app/router/router.go
@@ -3,11 +3,12 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/sjm1327605995/mycards_store/app/handler"
+	"github.com/sjm1327605995/mycards_store/app/handler/middleware"
 )
 
 func Router() (r *gin.Engine) {
 	e := gin.New()
-
+	e.Use(middleware.Cors())
 	api := e.Group("api")
 	{
 		api.POST("putDesks", handler.PutDesks)
